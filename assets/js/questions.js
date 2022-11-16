3a1bbedcdf9aead884d27a487b54f33e983b9984
ParkerCF <faul.parker@outlook.com>
2022-11-16 10:54:58 -0500

    adding js to project

---
+++ assets/js/questions.js
@@ -0,0 +1,37 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+      title: 'Inside which HTML element do we put the JavaScript?',
+      choices: ['<script>', '<javascript>', '<js>', '<scripting>'],
+      answer: '<script>',
+    },
+    {
+      title: 'Where is the correct place to insert a JavaScript?',
+      choices: ['The <body> section', 'Either the <head> section and the <body> section',
+       'The <head> section', 'nowhere'],
+      answer: 'Either the <head> section and the <body> section',
+    },
+    {
+      title: 'How do you create a function in JavaScript?',
+      choices: [
+        'function:myFunction()',
+        'function = myFunction()',
+        'function myFunction()',
+        'this is a function (myFunction)',
+      ],
+      answer: 'function myFunction()',
+    },
+    {
+      title:
+        'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
+      choices: ['if i <> 5', 'if i =! 5 then', 'if (i <> 5)', 'if (i != 5)'],
+      answer: 'if (i != 5)',
+    },
+    {
+      title:
+        'How can you add a comment in a JavaScript?',
+      choices: ['This is a comment', '<!--This is a comment-->', '//This is a comment', '~~~~Comment~~~~'],
+      answer: '//This is a comment',
+    },
+  ];
+  
