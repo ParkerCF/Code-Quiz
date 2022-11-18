ea5ff57dd4e19112eff1f6e61fbc4e730ddf2f33
ParkerCF <faul.parker@outlook.com>
2022-11-18 11:36:32 -0500

    updating js code

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,34 +14,28 @@ var feedbackEl = document.getElementById('feedback');
 
 
 function startQuiz() {
-  // hide start screen
   var startScreenEl = document.getElementById('start-screen');
   startScreenEl.setAttribute('class', 'hide');
 
-  // un-hide questions section
   questionsEl.removeAttribute('class');
 
-  // start timer
   timerId = setInterval(clockTick, 1000);
 
-  // show starting time
   timerEl.textContent = time;
 
   getQuestion();
 }
 
 function getQuestion() {
-  // get current question object from array
+  // get question
   var currentQuestion = questions[index];
 
-  // update title with current question
   var titleEl = document.getElementById('question-title');
   titleEl.textContent = currentQuestion.title;
 
-  // clear out any old question choices
+  // clear any old choices 
   choicesEl.innerHTML = '';
 
-  // loop over choices
   for (var i = 0; i < currentQuestion.choices.length; i++) {
     // create new button for each choice
     var choice = currentQuestion.choices[i];
@@ -51,7 +45,6 @@ function getQuestion() {
 
     choiceNode.textContent = i + 1 + '. ' + choice;
 
-    // display on the page
     choicesEl.appendChild(choiceNode);
   }
 }
@@ -59,21 +52,19 @@ function getQuestion() {
 function questionClick(event) {
   var buttonEl = event.target;
 
-  // if the clicked element is not a choice button, do nothing.
   if (!buttonEl.matches('.choice')) {
     return;
   }
 
   // check if user guessed wrong
   if (buttonEl.value !== questions[index].answer) {
-    // penalize time
+    // take away time
     time -= 15;
 
     if (time < 0) {
       time = 0;
     }
 
-    // display new time on page
     timerEl.textContent = time;
 
     feedbackEl.textContent = 'Wrong!';
@@ -82,16 +73,14 @@ function questionClick(event) {
     feedbackEl.textContent = 'Correct!';
   }
 
-  // flash right/wrong feedback on page for half a second
+  // flash feedback on page
   feedbackEl.setAttribute('class', 'feedback');
   setTimeout(function () {
     feedbackEl.setAttribute('class', 'feedback hide');
   }, 1000);
 
-  // move to next question
   index++;
 
-  // check if we've run out of questions
   if (time <= 0 || index === questions.length) {
     quizEnd();
   } else {
@@ -100,7 +89,6 @@ function questionClick(event) {
 }
 
 function quizEnd() {
-  // stop timer
   clearInterval(timerId);
 
   // show end screen
@@ -111,32 +99,30 @@ function quizEnd() {
   var finalScoreEl = document.getElementById('final-score');
   finalScoreEl.textContent = time;
 
-  // hide questions section
   questionsEl.setAttribute('class', 'hide');
 }
 
 function clockTick() {
-  // update time
+
   time--;
   timerEl.textContent = time;
 
-  // check if user ran out of time
+//check to see if time remains
   if (time <= 0) {
     quizEnd();
   }
 }
 
 function saveHighscore() {
-  // get value of input box
+  // input value
   var initials = initialsEl.value.trim();
 
-  // make sure value wasn't empty
+  // check if empty
   if (initials !== '') {
-    // get saved scores from localstorage, or if not any, set to empty array
+    // read saved scores
     var highscores =
       JSON.parse(window.localStorage.getItem('highscores')) || [];
 
-    // format new score object for current user
     var newScore = {
       score: time,
       initials: initials,
@@ -146,18 +132,18 @@ function saveHighscore() {
     highscores.push(newScore);
     window.localStorage.setItem('highscores', JSON.stringify(highscores));
 
-    // redirect to next page
+    // next page 
     window.location.href = 'high-scores.html';
   }
 }
 
 
-// user clicks button to submit initials
+// user submit initials
 submitBtn.onclick = saveHighscore;
 
-// user clicks button to start quiz
+// user start quiz
 startBtn.onclick = startQuiz;
 
-// user clicks on element containing choices
+// user clicks on a choice
 choicesEl.onclick = questionClick;
 
--- assets/js/high-scores.js
+++ assets/js/high-scores.js
@@ -1,14 +1,14 @@
 function printHighscores() {
-  // either get scores from localstorage or set to empty array
+
   var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
 
-  // sort highscores by score property in descending order
+  // sort descending
   highscores.sort(function (a, b) {
     return b.score - a.score;
   });
 
   for (var i = 0; i < highscores.length; i += 1) {
-    // create li tag for each high score
+    
     var liTag = document.createElement('li');
     liTag.textContent = highscores[i].initials + ' - ' + highscores[i].score;
 
@@ -25,5 +25,5 @@ function clearHighscores() {
 
 document.getElementById('clear').onclick = clearHighscores;
 
-// run function when page loads
+// run when page loads 
 printHighscores();
